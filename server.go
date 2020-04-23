3f102b71a556ddd65de0bda47b024421f420b126
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2020-04-22 22:42:29 -0500

    Creating and Implementing a Server Router
    Handling root /

--- server.go
+++ server.go
@@ -5,16 +5,19 @@ import (
 )
 
 type Server struct {
-	port string
+	port   string
+	router *Router
 }
 
 func NewServer(port string) *Server {
 	return &Server{
-		port: port,
+		port:   port,
+		router: NewRouter(),
 	}
 }
 
 func (s *Server) Listen() error {
+	http.Handle("/", s.router)
 	err := http.ListenAndServe(s.port, nil)
 	if err != nil {
 		return err
