69f85cb7419fe1727afd59bda8124215baf7faab
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2020-04-24 15:55:43 -0500

    Creating User and MetaData Types and Creating UserPostRequest Functionality
    Adding /user POST Endpoint
    Creating PostRequest Handler

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,34 @@ func HandleRoot(w http.ResponseWriter, request *http.Request) {
 
 func HandleApi(w http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(w, "API Endpoint...")
+}
+
+func PostRequest(w http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+	// fmt.Println(user.FirstName)
+	// fmt.Fprintf(w, "Payload %v\n", user)
 }
--- types.go
+++ types.go
@@ -1,5 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+type MetaData interface{}
+
+type User struct {
+	Id        int    `json:"id"`
+	Nickname  string `json:"nickname"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+}
+
+func (user *User) ToJson() ([]byte, error) {
+	return json.Marshal(user)
+}
